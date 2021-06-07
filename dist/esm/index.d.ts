d7ff466d46a63e6951722b25e467dabfba949549
hs@DEVELOPER02 <h.schindler@qs-schindler.de>
2021-06-07 14:50:34 +0200

    - changed gitignore to push dist directory

---
+++ dist/esm/index.d.ts
@@ -0,0 +1,4 @@
+import type { C9GunApiCapacitorPlugin } from './definitions';
+declare const C9GunApiCapacitor: C9GunApiCapacitorPlugin;
+export * from './definitions';
+export { C9GunApiCapacitor };
