2d235a4dfd8cd4b51f9ed01f86f1e751c6e4d8fa
hs@NOTEBOOK-HS <h.schindler@qs-schindler.de>
2023-07-18 10:07:22 +0200

    - increased version to 2.2.0 - migrated to capacitor v5 and updated all packages to latest

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { C9GunApiCapacitorPlugin } from './definitions';
-declare const C9GunApiCapacitor: C9GunApiCapacitorPlugin;
-export * from './definitions';
-export { C9GunApiCapacitor };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const C9GunApiCapacitor = registerPlugin('C9GunApiCapacitorPlugin', {
-    web: () => import('./web').then(m => new m.C9GunApiCapacitorPluginWeb()),
-});
-export * from './definitions';
-export { C9GunApiCapacitor };
-//# sourceMappingURL=index.js.map
