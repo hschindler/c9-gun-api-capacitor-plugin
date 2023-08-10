b9170b6161de164437b9901a3d83086b52e38619
hs@NOTEBOOK-HS <h.schindler@qs-schindler.de>
2023-08-10 15:08:39 +0200

    - added dist to repository

---
+++ dist/esm/web.js
@@ -0,0 +1,48 @@
+import { WebPlugin } from '@capacitor/core';
+export class C9GunApiCapacitorPluginWeb extends WebPlugin {
+    async echo(options) {
+        console.log('ECHO', options);
+        return options;
+    }
+    async getFirmware() {
+        // logic here
+        this.throwUnimplementedError();
+    }
+    async startBarcodeInventory() {
+        // logic here
+        this.throwUnimplementedError();
+    }
+    async stopBarcodeInventory() {
+        // logic here
+        this.throwUnimplementedError();
+    }
+    async setBarcodeTimeout() {
+        this.throwUnimplementedError();
+    }
+    async startInventory() {
+        // logic here
+        this.throwUnimplementedError();
+    }
+    async stopInventory() {
+        // logic here
+        this.throwUnimplementedError();
+    }
+    async setOutputPower() {
+        // logic here
+        this.throwUnimplementedError();
+    }
+    async getOutputPower() {
+        // logic here
+        this.throwUnimplementedError();
+    }
+    async writeEPCToTagByEPC() {
+        // logic here
+        this.throwUnimplementedError();
+    }
+    throwUnimplementedError() {
+        throw this.unimplemented('Not implemented on web.');
+    }
+}
+// const C9GunApiCapacitorPlugin = new C9GunApiCapacitorPluginWeb();
+// export { C9GunApiCapacitorPlugin };
+//# sourceMappingURL=web.js.map
